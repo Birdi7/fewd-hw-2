eca63b0da35a1e8a818a871f9ff5211f614b9942
Egor <egor.osokin7@gmail.com>
2021-09-16 20:49:00 +0300

    feat: smth, check pre-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function Main() {
+  const [state, setState] = useState({ inp: "", submitted: "" });
+
+  function HandleSubmit(event) {
+    event.preventDefault();
+
+    setState({ submitted: state.inp, inp: "" });
+  }
+
+  return (
+    <form onSubmit={HandleSubmit}>
+      <label>
+        <p> Name: {state.submitted} </p>
+        <input
+          type="text"
+          name="name"
+          onChange={(event) => setState({ ...state, inp: event.target.value })}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Main />
       </header>
     </div>
   );
